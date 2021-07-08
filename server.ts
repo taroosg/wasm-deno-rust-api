6e052d7580bf441a984a1dbcee09fec7b9cb9c7f
taroosg <root@taroosg.dev>
2021-07-08 11:18:57 +0900

    test deploy

--- server.ts
+++ server.ts
@@ -17,8 +17,8 @@ router
   })
   .get("/:number", (context) => {
     console.log(context.params);
-    // const res = { result: fib(Number(context.params.number)).toString() };
-    const res = { result: Number(context.params.number).toString() };
+    const res = { result: fib(Number(context.params.number)).toString() };
+    // const res = { result: Number(context.params.number).toString() };
     // const res = { result: 42 };
     context.response.body = JSON.stringify(res);
   });
